77c57a3600e3eaddaa5c09916284adc2979d0bdd
wada <wada@avita.co.jp>
2022-07-30 21:51:03 +0900

    add log output

--- examples/react-native-app/App.js
+++ examples/react-native-app/App.js
@@ -3,10 +3,16 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  // define a function to print "clicked!"
+  const onPress = () => {
+    console.log('clicked!');
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text onPress={onPress}>Click me!</Text>
     </View>
   );
 }
