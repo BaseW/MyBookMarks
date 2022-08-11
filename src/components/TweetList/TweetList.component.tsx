f142016df6ee3fb073ba5d94c737b5b3dbe53770
wada <yakyugo51@gmail.com>
2022-08-11 13:35:28 +0900

    storybook with scss

--- src/components/TweetList/TweetList.component.tsx
+++ src/components/TweetList/TweetList.component.tsx
@@ -1,4 +1,5 @@
 import { TweetListProps } from "./TweetList.types";
+import styles from "./TweetList.module.scss";
 
 export const TweetList = ({ tweets }: TweetListProps) => {
   return (
@@ -6,7 +7,7 @@ export const TweetList = ({ tweets }: TweetListProps) => {
       {
         tweets.map((tweet) => {
           return (
-            <div key={tweet.id}>
+            <div key={tweet.id} className={styles.container}>
               <h3>{tweet.author}</h3>
               <p>{tweet.content}</p>
             </div>
